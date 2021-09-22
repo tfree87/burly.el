2f5ae75e76b7e8216217fe79db3621b1bd622d16
Adam Porter <adam@alphapapa.net>
2021-09-22 04:40:59 +0000

    WIP: Depend on Emacs 28.0 for new tab-bar features
    Thanks to Juri Linkov (@link0ff) for implementing them.

--- burly.el
+++ burly.el
@@ -5,7 +5,7 @@
 ;; Author: Adam Porter <adam@alphapapa.net>
 ;; URL: https://github.com/alphapapa/burly.el
 ;; Version: 0.2-pre
-;; Package-Requires: ((emacs "26.3") (map "2.1"))
+;; Package-Requires: ((emacs "28.0") (map "2.1"))
 ;; Keywords: convenience
 
 ;; This program is free software; you can redistribute it and/or modify
